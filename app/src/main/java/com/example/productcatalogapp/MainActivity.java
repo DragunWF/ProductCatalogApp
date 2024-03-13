0091feb2ec92eb63cef9086d45d9a03d8e7e5f2b
dragunwf <plarisanmarc2@gmail.com>
2024-03-13 11:11:42 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/productcatalogapp/MainActivity.java
+++ app/src/main/java/com/example/productcatalogapp/MainActivity.java
@@ -4,20 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "Products App";
     private Button addButton;
+    private List<Product> productList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fillProductList();
+        Log.d(TAG, String.format("onCreate: %s", productList.toString()));
+        Log.d(TAG, String.valueOf(productList.size()));
+
         addButton = findViewById(R.id.addButton);
 
         setButtons();
+    }
+
+    private void fillProductList() {
+        Product[] initialProducts = {
+                new Product(1, "Apple", "One keeps the doctor away", 23.53),
+                new Product(2, "Book", "Knowledge is power", 252.21),
+                new Product(3, "Frying Pan", "It twas' quite famous in PUBG back at 2020", 153.12)
+        };
+        productList.addAll(Arrays.asList(initialProducts));
     }
 
     private void setButtons() {
