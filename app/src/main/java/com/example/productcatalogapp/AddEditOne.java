64177070cc9fe585a28fdcd9423857152e9ccbf5
dragunwf <plarisanmarc2@gmail.com>
2024-03-13 10:07:13 +0800

    Update AddEditOne.java

--- app/src/main/java/com/example/productcatalogapp/AddEditOne.java
+++ app/src/main/java/com/example/productcatalogapp/AddEditOne.java
@@ -2,13 +2,33 @@ package com.example.productcatalogapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AddEditOne extends AppCompatActivity {
+    private Button createButton;
+    private Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_one);
+
+        createButton = findViewById(R.id.createButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+        setButtons();
+    }
+
+    private void setButtons() {
+        createButton.setOnClickListener(v -> {
+
+        });
+
+        cancelButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
