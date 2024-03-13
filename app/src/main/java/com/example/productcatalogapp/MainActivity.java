3137e547ab4b938c6b8a0dbd57bc284d9a12ee46
dragunwf <plarisanmarc2@gmail.com>
2024-03-13 10:07:20 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/productcatalogapp/MainActivity.java
+++ app/src/main/java/com/example/productcatalogapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.productcatalogapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.addButton);
+
+        setButtons();
+    }
+
+    private void setButtons() {
+        addButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AddEditOne.class);
+            startActivity(intent);
+        });
     }
 }
