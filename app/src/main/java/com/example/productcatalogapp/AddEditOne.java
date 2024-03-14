c1a1b8c14d184151dc0d1f44546bb6ba70d09004
dragunwf <plarisanmarc2@gmail.com>
2024-03-14 20:39:32 +0800

    Update AddEditOne.java

--- app/src/main/java/com/example/productcatalogapp/AddEditOne.java
+++ app/src/main/java/com/example/productcatalogapp/AddEditOne.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AddEditOne extends AppCompatActivity {
     private Button createButton;
     private Button cancelButton;
+    private EditText editTextProductName;
+    private EditText editTextProductDescription;
+    private EditText editTextProductPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +22,33 @@ public class AddEditOne extends AppCompatActivity {
         createButton = findViewById(R.id.createButton);
         cancelButton = findViewById(R.id.cancelButton);
 
+        editTextProductName = findViewById(R.id.editTextProductName);
+        editTextProductDescription = findViewById(R.id.editTextProductDescription);
+        editTextProductPrice = findViewById(R.id.editTextProductPrice);
+
         setButtons();
     }
 
     private void setButtons() {
         createButton.setOnClickListener(v -> {
-
+            MainActivity.productList.add(
+                    new Product(
+                            1,
+                            String.valueOf(editTextProductName.getText()),
+                            String.valueOf(editTextProductDescription.getText()),
+                            Double.parseDouble(String.valueOf(editTextProductPrice.getText()))
+                    )
+            );
+            switchViews();
         });
 
         cancelButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            switchViews();
         });
+    }
+
+    private void switchViews() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
